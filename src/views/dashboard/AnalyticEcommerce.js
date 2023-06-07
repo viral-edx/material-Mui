d515e697ea0ab260914c6137053d348b0b73c431
Dev-edx <d.bhuptani@edexa.team>
2023-06-07 18:50:22 +0530

    analytic ecommerce page

--- src/views/dashboard/AnalyticEcommerce.js
+++ src/views/dashboard/AnalyticEcommerce.js
@@ -1,30 +1,43 @@
 import PropTypes from 'prop-types';
 import { Box, Chip, Grid, Stack, Typography } from '@mui/material';
 import MainCard from './Default/MainCard';
+import { TrendingDown, TrendingUp } from '@mui/icons-material';
 
 // ==============================|| STATISTICS - ECOMMERCE CARD  ||============================== //
 
-const AnalyticEcommerce = ({ color, title, count, percentage, extra }) => (
-  <MainCard contentSX={{ p: 2.25 }}>
+const AnalyticEcommerce = ({ color, title, count, percentage, extra, isLoss }) => (
+  <MainCard contentSX={{ p: 3 }}>
     <Stack spacing={0.5}>
-      <Typography variant="h6" color="textSecondary">
+      <Typography variant="h6" color="textSecondary" className="title-typography">
         {title}
       </Typography>
       <Grid container alignItems="center">
         <Grid item>
-          <Typography variant="h4" color="inherit">
+          <Typography variant="h4" color="inherit" className="count-typography">
             {count}
           </Typography>
         </Grid>
         {percentage && (
           <Grid item>
-            <Chip variant="combined" color={color} label={`${percentage}%`} sx={{ ml: 1.25, pl: 1 }} size="small" />
+            <Chip
+              variant="combined"
+              color={color}
+              icon={
+                <>
+                  {!isLoss && <TrendingUp style={{ fontSize: '0.75rem', color: 'inherit' }} />}
+                  {isLoss && <TrendingDown style={{ fontSize: '0.75rem', color: 'inherit' }} />}
+                </>
+              }
+              label={`${percentage}%`}
+              sx={{ ml: 1.25, pl: 1, borderRadius: '3px', borderColor: `${color || 'primary'}.main` }}
+              size="small"
+            />
           </Grid>
         )}
       </Grid>
     </Stack>
-    <Box sx={{ pt: 2.25 }}>
-      <Typography variant="caption" color="textSecondary">
+    <Box sx={{ pt: 3 }}>
+      <Typography variant="caption" color="#8c8c8c">
         You made an extra{' '}
         <Typography component="span" variant="caption" sx={{ color: `${color || 'primary'}.main` }}>
           {extra}
--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import AnalyticEcommerce from '../AnalyticEcommerce';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
@@ -8,9 +8,6 @@ const Dashboard = () => {
     <>
       <Grid container rowSpacing={4.5} columnSpacing={2.75}>
         {/* row 1 */}
-        <Grid item xs={12} sx={{ mb: -2.25 }}>
-          <Typography variant="h5">Dashboard</Typography>
-        </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
           <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
         </Grid>
