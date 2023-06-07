951e191b80c5f762870214e793e61380ddebde90
Viral Brahmbhatt <v.brahmbhatt@edexa.team>
2023-06-07 18:51:21 +0530

    dashboard changes

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,12 +1,37 @@
 // import { useEffect, useState } from 'react';
-
-import { Grid, Typography } from '@mui/material';
+import { Grid, MenuItem, Stack, TextField, Typography } from '@mui/material';
 import AnalyticEcommerce from '../AnalyticEcommerce';
+import MainCard from './MainCard';
+import SalesColumnChart from './SalesColumnChart';
+// import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+// import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import { useState, useEffect } from 'react';
+import PopularCards from './PopularCardS';
+// import BajajAreaChartCard from './BajajAreaChartCard.client';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
-
+  // const theme = useTheme();
+  const status = [
+    {
+      value: 'today',
+      label: 'Today'
+    },
+    {
+      value: 'month',
+      label: 'This Month'
+    },
+    {
+      value: 'year',
+      label: 'This Year'
+    }
+  ];
+  const [value, setValue] = useState('today');
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     // <>
     //   <Grid container spacing={gridSpacing}>
@@ -59,6 +84,42 @@ const Dashboard = () => {
       </Grid>
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Total Sales" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" />
+      </Grid>
+
+      <Grid item xs={12} md={7} lg={8}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Sales Report</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="standard-select-currency"
+              size="small"
+              select
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              sx={{ '& .MuiInputBase-input': { py: 0.5, fontSize: '0.875rem' } }}
+            >
+              {status.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        </Grid>
+        <MainCard sx={{ mt: 1.75 }}>
+          <Stack spacing={1.5} sx={{ mb: -12 }}>
+            <Typography variant="h6" color="secondary">
+              Net Profit
+            </Typography>
+            <Typography variant="h4">$1560</Typography>
+          </Stack>
+          <SalesColumnChart />
+        </MainCard>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <PopularCards isLoading={isLoading} />
       </Grid>
     </Grid>
   );
